e053273574e4f61430b3792756616c4c1a2a473c
Renato Teixeira <rteixeir@progress.com>
2016-06-22 14:34:54 +0000

    Changes made by Renato Teixeira (rteixeir@progress.com).

--- Progress Exchange/components/conferences/index.js
+++ Progress Exchange/components/conferences/index.js
@@ -1,140 +1,153 @@
 'use strict';
 
 app.conferences = kendo.observable({
-    afterShow: function() {
-        // var model = app.conferences.conferencesModel
-        // if(model && model.dataSource) {
-        //     model.dataSource.filter({ field: "Event", operator: "eq", value: app.currentEvent.Id });
-        // }
-    }
+    afterShow: function () {}
 });
 
 // START_CUSTOM_CODE_conferences
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
 // END_CUSTOM_CODE_conferences
-(function(parent) {
+(function (parent) {
     var dataProvider = app.data.progressExchange2016Novo,
-    fetchFilteredData = function(paramFilter, searchFilter) {
-        var model = parent.get('conferencesModel'),
-        dataSource = model.get('dataSource');
+        fetchFilteredData = function (paramFilter, searchFilter) {
+            var model = parent.get('conferencesModel'),
+                dataSource = model.get('dataSource');
 
-        if (paramFilter) {
-            model.set('paramFilter', paramFilter);
-        } else {
-            model.set('paramFilter', undefined);
-        }
-
-        if (paramFilter && searchFilter) {
-            dataSource.filter({
-                logic: 'and',
-                filters: [paramFilter, searchFilter]
-            });
-        } else if (paramFilter || searchFilter) {
-            dataSource.filter(paramFilter || searchFilter);
-        } else {
-            dataSource.filter({});
-        }
-    },
-    processImage = function(img) {
-        if (!img) {
-            var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
-            img = 'data:image/png;base64,' + empty1x1png;
-        } else if (img.slice(0, 4) !== 'http' &&
-            img.slice(0, 2) !== '//' && img.slice(0, 5) !== 'data:') {
-            var setup = dataProvider.setup || {};
-            img = setup.scheme + ':' + setup.url + setup.appId + '/Files/' + img + '/Download';
-        }
-
-        return img;
-    },
-    flattenLocationProperties = function(dataItem) {
-        var propName, propValue,
-        isLocation = function(value) {
-            return propValue && typeof propValue === 'object' &&
-            propValue.longitude && propValue.latitude;
-        };
-
-        for (propName in dataItem) {
-            if (dataItem.hasOwnProperty(propName)) {
-                propValue = dataItem[propName];
-                if (isLocation(propValue)) {
-                    dataItem[propName] =
-                    kendo.format('Latitude: {0}, Longitude: {1}',
-                        propValue.latitude, propValue.longitude);
-                }
+            if (paramFilter) {
+                model.set('paramFilter', paramFilter);
+            } else {
+                model.set('paramFilter', undefined);
             }
-        }
-    },
-    dataSourceOptions = {
-        type: 'everlive',
-        transport: {
-            typeName: 'Conference',
-            dataProvider: dataProvider
-        },
-        group: {
-            field: 'Track'
-        },
-        change: function(e) {
-            var data = this.data();
-            for (var i = 0; i < data.length; i++) {
-                var dataItem = data[i];
 
-                flattenLocationProperties(dataItem);
+            if (paramFilter && searchFilter) {
+                dataSource.filter({
+                    logic: 'and',
+                    filters: [paramFilter, searchFilter]
+                });
+            } else if (paramFilter || searchFilter) {
+                dataSource.filter(paramFilter || searchFilter);
+            } else {
+                dataSource.filter({});
             }
         },
-        error: function(e) {
-            if (e.xhr) {
-                alert(JSON.stringify(e.xhr));
+        processImage = function (img) {
+            if (!img) {
+                var empty1x1png = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQI12NgYAAAAAMAASDVlMcAAAAASUVORK5CYII=';
+                img = 'data:image/png;base64,' + empty1x1png;
+            } else if (img.slice(0, 4) !== 'http' &&
+                img.slice(0, 2) !== '//' && img.slice(0, 5) !== 'data:') {
+                var setup = dataProvider.setup || {};
+                img = setup.scheme + ':' + setup.url + setup.appId + '/Files/' + img + '/Download';
             }
+
+            return img;
         },
-        schema: {
-            model: {
-                fields: {
-                    'Name': {
-                        field: 'Name',
-                        defaultValue: ''
-                    },
-                    'Time': {
-                        field: 'Time',
-                        defaultValue: ''
-                    },
-                },
-                iconTime: function() {
-                    var i = 'time';
-                    return kendo.format('km-icon km-{0}', i);
-                },
-                iconPresenter: function() {
-                    var i = 'presenter';
-                    return kendo.format('km-icon km-{0}', i);
+        flattenLocationProperties = function (dataItem) {
+            var propName, propValue,
+                isLocation = function (value) {
+                    return propValue && typeof propValue === 'object' &&
+                        propValue.longitude && propValue.latitude;
+                };
+
+            for (propName in dataItem) {
+                if (dataItem.hasOwnProperty(propName)) {
+                    propValue = dataItem[propName];
+                    if (isLocation(propValue)) {
+                        dataItem[propName] =
+                            kendo.format('Latitude: {0}, Longitude: {1}',
+                                propValue.latitude, propValue.longitude);
+                    }
                 }
             }
         },
-        serverFiltering: true,
-    },
-    dataSource = new kendo.data.DataSource(dataSourceOptions),
-    conferencesModel = kendo.observable({
-        dataSource: dataSource,
-        itemClick: function(e) {
+        dataSourceOptions = {
+            type: 'everlive',
+            transport: {
+                typeName: 'Conference',
+                dataProvider: dataProvider
+            },
+            group: {
+                field: 'Time'
+            },
+            change: function (e) {
+                var data = this.data();
+                for (var i = 0; i < data.length; i++) {
+                    var dataItem = data[i];
 
-            app.mobileApp.navigate('#components/conferences/details.html?uid=' + e.dataItem.uid);
-
+                    flattenLocationProperties(dataItem);
+                }
+            },
+            error: function (e) {
+                if (e.xhr) {
+                    alert(JSON.stringify(e.xhr));
+                }
+            },
+            schema: {
+                model: {
+                    fields: {
+                        'Name': {
+                            field: 'Name',
+                            defaultValue: ''
+                        },
+                        'Time': {
+                            field: 'Time',
+                            defaultValue: ''
+                        },
+                    },
+                    iconTime: function () {
+                        var i = 'time';
+                        return kendo.format('km-icon km-{0}', i);
+                    },
+                    iconPresenter: function () {
+                        var i = 'presenter';
+                        return kendo.format('km-icon km-{0}', i);
+                    }
+                }
+            },
+            serverFiltering: true,
         },
-        detailsShow: function(e) {
-            var item = e.view.params.uid,
-            dataSource = conferencesModel.get('dataSource'),
-            itemModel = dataSource.getByUid(item);
-            itemModel.PhotoUrl = processImage(itemModel.Photo);
+        dataSource = new kendo.data.DataSource(dataSourceOptions),
+        conferencesModel = kendo.observable({
+            dataSource: dataSource,
+            itemClick: function (e) {
 
-            if (!itemModel.Name) {
-                itemModel.Name = String.fromCharCode(160);
-            }
+                app.mobileApp.navigate('#components/conferences/details.html?uid=' + e.dataItem.uid);
 
-            conferencesModel.set('currentItem', null);
-            conferencesModel.set('currentItem', itemModel);
-        },
-        currentItem: null
-    });
+            },
+            rateShow: function (e) {
+                // target element
+                var el = $('#ratingStars');
+
+                // current rating, or initial rating
+                var currentRating = 0;
+
+                // max rating, i.e. number of stars you want
+                var maxRating = 5;
+
+                // callback to run after setting the rating
+                var callback = function (rating) {
+                    alert(rating);
+                };
+
+                // rating instance
+                var myRating = rating(el, currentRating, maxRating, callback);
+            },
+            detailsShow: function (e) {
+                var item = e.view.params.uid,
+                    dataSource = conferencesModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                itemModel.PhotoUrl = processImage(itemModel.Photo);
+
+                if (!itemModel.Name) {
+                    itemModel.Name = String.fromCharCode(160);
+                }
+
+                conferencesModel.set('currentItem', null);
+                conferencesModel.set('currentItem', itemModel);
+            },
+            currentItem: null
+        });
 
     if (typeof dataProvider.sbProviderReady === 'function') {
         dataProvider.sbProviderReady(function dl_sbProviderReady() {
@@ -144,8 +157,12 @@ app.conferences = kendo.observable({
         parent.set('conferencesModel', conferencesModel);
     }
 
-    parent.set('onShow', function(e) {
-        var param = { field: "Event", operator: "eq", value: app.currentEvent.Id }; 
+    parent.set('onShow', function (e) {
+        var param = {
+            field: "Event",
+            operator: "eq",
+            value: app.currentEvent.Id
+        };
         //e.view.params.filter ? JSON.parse(e.view.params.filter) : null;
 
         fetchFilteredData(param);
